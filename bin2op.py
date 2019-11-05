4ebb35e0647d44643364a840ba9ad469daf51c22
deniz <d3n1z@protonmail.com>
2019-11-05 05:25:43 +0300

    vector_representation_of_sentences

--- bin2op.py
+++ bin2op.py
@@ -8,124 +8,35 @@ Extract the opcode from the objdump of a binary
 import re
 import os
 import sys
-import getopt
+import argparse
 import string
 from subprocess import check_output
 from collections import Counter
-
-__version__ = "$Id: bin2op.py,v 1.2 2015/12/28 13:21:32 dhn Exp $"
-__license__ = "BSD"
+import re
 
 
-def getopts():
-    try:
-        usage_opt = [
-                        "file=", "short", "large", "opcode", "intel",
-                        "att", "python", "version", "help"
-                    ]
+shellcode=None
+code=None
+opcodes=None
+operands=None
 
-        opts, args = getopt.getopt(sys.argv[1:], "f:sloiapvh", usage_opt)
-
-    except getopt.GetoptError as err:
-        print(str(err))
-        usage()
-        sys.exit(2)
-    objfile = None
-    shellcode = None
-    code = None
-    syntax = "intel"
-    formats = None
-    for opt, arg in opts:
-        if opt in ("-i", "--intel"):
-            syntax = "intel"
-        elif opt in ("-a", "--att"):
-            syntax = "att"
-        elif opt in ("-p", "--python"):
-            formats = "python"
-        elif opt in ("-f", "--file"):
-            objfile = arg
-            if os.path.exists(objfile):
-                shellcode, code, opcodes, operands = parse(objfile, syntax, formats)
-            else:
-                print("[!] file does not exist")
-                sys.exit(1)
-        elif opt in ("-s", "--short"):
-            if objfile is not None:
-                if formats is not None:
-                    shellcode = re.sub(
-                                "(.{32})", "\tb\"\\1\"\n",
-                                shellcode, 0, re.DOTALL)
-                    print("shellcode = (")
-                    print(shellcode[:-1])
-                    print(")")
-                else:
-                    shellcode = re.sub(
-                                "(.{32})", "\\1\n",
-                                shellcode, 0, re.DOTALL)
-                    print(shellcode[:-1])
-        elif opt in ("-l", "--large"):
-            if objfile is not None:
-                # FIXME: this is really ugly :(
-                if formats is not None:
-                    print("shellcode = (")
-                for line in code:
-                    break #print("", line)
-                if formats is not None:
-                    print(")")
-        elif opt in ("-o", "--opcode"):
-            if objfile is not None:
-                # FIXME: this is really ugly :(
-                unique(opcodes,True)
-                print("\n")
-                print(counts(opcodes))
-                if formats is not None:
-                    print("opcodes = (")
-                for opcode in opcodes:
-                    print("", opcode)
-                if formats is not None:
-                    print(")")    
-                if formats is not None:
-                    print("operands = (")
-                for operand in operands:
-                    print("", operand)
-                if formats is not None:
-                    print(")")    
-                
-        elif opt in ("-h", "--help"):
-            usage()
-        elif opt in ("-v", "--version"):
-            print("%s - %s" % (__version__, __license__))
-        else:
-            assert False, "unhandled option"
-
-
-def usage():
-    print("usage: %s [options]" % __file__)
-    print("   -f, --file     The assembly code filename")
-    print("   -s, --short    Show less version of opcode")
-    print("   -l, --large    Show verbose version of opcode")
-    print("   -i, --intel    Use the intel assembly syntax")
-    print("   -a, --att      Use the AT&T assembly syntax")
-    print("   -p, --python   Format output to python syntax")
-    print("")
-    print("Example: %s -a -f bindshell/build/bindshell.o -l" % __file__)
-    print("         %s -f bindshell/build/bindshell.o -s" % __file__)
-    print("         %s -p -f bindshell/build/bindshell.o -s" % __file__)
-
-def nextIndex(string: str, text: str, start: int=0):
+def nextIndex(needle, haystack, start: int=0):
     while True:
         try:
-            start = text.index(string, start)
+            start = haystack.index(needle, start)
             yield start
             start += 1
         except ValueError:
             raise StopIteration
 
-def counts(list):
-    #return dict((elem, list.count(elem)) for elem in list)
-    return Counter(list)
+def counts(list,verbose=False):
+    #return dict((elem, list.count(elem)) for elem in list)   #this takes too much time
+    if verbose:
+        print(Counter(list))
+    else:
+        return Counter(list)
 # function to get unique values 
-def unique(list1,verbose): 
+def unique(list1,verbose=False): 
   
     # intilize a null list 
     unique_list = [] 
@@ -141,6 +52,7 @@ def unique(list1,verbose):
             print(x, end=",") 
     else:
         return unique_list
+
 # thanks zerosum0x0
 def parse(obj, syntax, formats):
     objdump = ['objdump', '-d', '-M', syntax, obj]
@@ -151,11 +63,13 @@ def parse(obj, syntax, formats):
     
     lines = lines.split(b'\n')[3:]
 
+    replace_list = {'DWORD': 'A', 'QWORD': 'A', 'BYTE':'A',',':" "}
+
     shellcode = ""
     code = []
     opcodes = []
     operands = []
-
+    instructions = []
     for line in lines:
         line = line.strip()
 
@@ -167,12 +81,21 @@ def parse(obj, syntax, formats):
         instruction = "."
         if (len(tabs) == 3):
             instruction = tabs[2].strip().decode("utf-8")
-            instruction_split = instruction.replace('    ',' ').replace('   ',' ').replace('  ',' ').split(' ')
-            #print(instruction_split)
+            instruction_clean = instruction.replace(',',' ')
+            instruction_clean = re.sub(' +', ' ', instruction_clean)
+            instruction_clean = re.sub(r'([D|Q]{0,}WORD |BYTE )', 'A', instruction_clean)
+            instruction_clean = re.sub(r'(0x[0-9a-fA-F]+)(?:)?', 'addr', instruction_clean)
+            
+            
+            instructions.append(instruction_clean)
+            instruction_split = instruction_clean.split(' ')
             opcode = instruction_split[0] if len(instruction_split) > 0 else 'none' 
-            opcodes.append(opcode)                
-            operand = instruction_split[1] if len(instruction_split) > 1 else 'none' 
-            operands.append(operand)
+            opcodes.append(opcode)        
+            # if len(instruction_split) > 1 :
+            #     for operand in instruction_split[1:]:
+            #         operands.extend(operand.split(','))
+            operand = instruction_split[1:] if len(instruction_split) > 1 else 'none' 
+            operands.extend(operand)
         
         bytes = bytes.split(b' ')
         shellcodeline = ""
@@ -186,11 +109,75 @@ def parse(obj, syntax, formats):
             c = '%-*s/* %s */' % (32, '"'+shellcodeline+'"', instruction)
         code.append(c)
 
-    return shellcode, code, opcodes, operands
+    return shellcode, code, opcodes, operands, instructions
+
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         usage()
     else:
-        getopts()
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--intel', action='store_true')
+        parser.add_argument('-a', '--att', action='store_true')
+        parser.add_argument('-o', '--opcode', dest='opcode', action='store_true')
+        parser.add_argument('-u', '--uniqopcode', dest='uniqopcode', action='store_true')
+        parser.add_argument('-c', '--countopcode', dest='countopcode', action='store_true')
+        parser.add_argument('-p', '--operand' , dest='operand', action='store_true')
+        parser.add_argument('-w', '--ops' , dest='ops', action='store_true')
+        parser.add_argument('-s', '--sentence' ,dest='sentence', action='store_true')
+        parser.add_argument('-f', '--file' )
+        parser.add_argument('-v', dest='verbose', action='store_true')
+        args = parser.parse_args()
+        if args.intel:
+            syntax = 'intel'
+        if args.att :
+            syntax = 'att'  
+        if args.sentence:
+            sentence = True  
+        if args.file:
+            if os.path.exists(args.file):
+                shellcode, code, opcodes, operands, instructions = parse(args.file, syntax, None)
+                if args.opcode:
+                    print("-"*20+"opcodes"+"-"*20 +"\n")
+                    for opcode in opcodes:
+                        print(opcode, end=',')
+                if args.uniqopcode:
+                    print("-"*20+"unique_opcodes"+"-"*20 +"\n")
+                    unique_opcodes=unique(opcodes)
+                    for opcode in unique_opcodes:
+                        print("'"+opcode+"'", end=',')
+                    print("\n"+"-"*20+"unique_opcodes_length"+"-"*20 +"\n")
+                    print(len(unique_opcodes))
+                if args.countopcode:
+                    print("-"*20+"count_opcodes"+"-"*20 +"\n")
+                    counts(opcodes,True)
+                if args.operand:
+                    print("-"*20+"unique_operands"+"-"*20 +"\n")
+                    unique_operands=unique(operands)
+                    for operand in unique_operands:
+                        print("'"+operand+"'", end=',')
+                    print("\n"+"-"*20+"unique_operands_length"+"-"*20 +"\n")
+                    print(len(unique_operands))
+                if args.sentence:
+                    print("-"*20+"instructions"+"-"*20 +"\n")
+                    print("[")
+                    for instruction in instructions:
+                        print("'"+instruction+"'", end=',')
+                    print("]")
+                if args.ops:
+                    print("-"*20+"opcodes and operands"+"-"*20 +"\n")
+                    print("[")
+                    unique_operands=unique(operands)
+                    unique_opcodes=unique(opcodes)
+                    ops = unique(unique_operands + unique_opcodes)
+                    for op in ops:
+                        print("'"+op+"'", end=',')
+                    print("]")
+                    print(len(ops))
+            else:
+                print("[!] file does not exist")
+                sys.exit(1)
+
+
