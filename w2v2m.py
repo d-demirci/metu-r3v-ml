9f7f1e39e8599297978a0aaa4b56dec494633de2
deniz <d3n1z@protonmail.com>
2020-02-12 16:34:37 +0300

    added w2v2m

--- w2v2m.py
+++ w2v2m.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 if args.directory:
     directory = args.directory
 else:
-    print('need a file parameter exited')
+    print('need a directory parameter exited')
     sys.exit(0)
 
 for file in os.listdir(directory):
@@ -24,23 +24,9 @@ for file in os.listdir(directory):
         shellcode, code, opcodes, operands, instructions = parse(os.path.join(directory,file), syntax, None)
 
         sentences = instructions
-        # ops = unique(operands + opcodes)
-        # ops.sort()
-        # unique_ops_count = len(ops)
 
-        # for i, sentence in enumerate(sentences):
-        #     tokenized= []
-        #     for word in sentence.split(' '):
-        #         word = word.split('.')[0]
-        #         word = word.lower()
-        #         tokenized.append(word)
-        #     sentences[i] = tokenized
-
-        with open('/mnt/hgfs/VM_SHARED/extracted_asm/'+file, 'wb') as sentences_file:
+        with open('/mnt/hgfs/benign/'+file+'.asm', 'wb') as sentences_file:
             pickle.dump(sentences, sentences_file)
             print('file processed > {}'.format(file))
-            # model = word2vec.Word2Vec(sentences, workers = 1, size = 2, min_count = 1, window = 3, sg = 0)
-            # similar_word = model.wv.most_similar('add')[0]
-            # print("Most common word to add is: {}".format(similar_word[0]))
     except Exception as e:
-        print('file couldnt be processed > {}'.format(file))
+        print('file couldnt be processed > {} {} '.format(file, str(e)))
